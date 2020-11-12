51a339d634edafb2846371d22c5ad71687bda1d6
Jason Salaber <jcsalaber@hotmail.com>
2020-11-12 16:09:15 -0500

    moved method back to old spot

--- src/main/java/com/mparticle/kits/TaplyticsKit.java
+++ src/main/java/com/mparticle/kits/TaplyticsKit.java
@@ -149,6 +149,11 @@ public class TaplyticsKit extends KitIntegration
     }
 
     @Override
+    public void setUserIdentity(IdentityType identityType, String s) {
+        // no-op
+    }
+
+    @Override
     public void removeUserIdentity(MParticle.IdentityType identityType) {
         setUserIdentity(identityType, null);
     }
@@ -157,11 +162,6 @@ public class TaplyticsKit extends KitIntegration
     @Override
     public void removeUserAttribute(String attribute) {
         setUserAttribute(attribute, null);
-    }
-
-    @Override
-    public void setUserIdentity(IdentityType identityType, String s) {
-        // no-op
     }
 
     @Override
